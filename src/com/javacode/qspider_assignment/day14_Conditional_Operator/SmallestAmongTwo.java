cf16816955d9cc13e2ca57ac9513717d9773b233
Ashutosh Mishra <ashutosh2582@gmail.com>
2024-03-15 22:57:07 +0530

    day 12

--- src/com/javacode/qspider_assignment/day14_Conditional_Operator/SmallestAmongTwo.java
+++ src/com/javacode/qspider_assignment/day14_Conditional_Operator/SmallestAmongTwo.java
@@ -1,8 +1,8 @@
-package com.javacode.qspider_assignment.day12;
+package com.javacode.qspider_assignment.day14_Conditional_Operator;
 
 import java.util.Scanner;
 
-class SmallestValue{
+class SmallestAmongTwo {
 	public static void main(String[] args) {
 		System.out.print("Enter first number: ");
 		Scanner sc = new Scanner(System.in);
@@ -11,6 +11,7 @@ class SmallestValue{
 		int num2 = sc.nextInt();
 		// System.out.println((num1 > num2)? num2 + " is smallest value": num1 + " is smallest value ");
 		int min = (num1 > num2)? num2 : num1;
+//		int min = Math.min(num1, num2);
 		System.out.println(num1 +" , "+ num2 + " smallest among 2 number is "+ min);
 
 	}
