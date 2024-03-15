db0c18fe7cdac18059b38408c37dd81046fa83f5
Ashutosh Mishra <ashutosh2582@gmail.com>
2024-03-15 22:52:50 +0530

    day 12

--- src/com/javacode/qspider_assignment/day14_Conditional_Operator/ArrayShortMinMax.java
+++ src/com/javacode/qspider_assignment/day14_Conditional_Operator/ArrayShortMinMax.java
@@ -1,4 +1,4 @@
-package com.javacode.qspider_assignment.day12;
+package com.javacode.qspider_assignment.day14_Conditional_Operator;
 
 public class ArrayShortMinMax {
     public static void main(String[] args) {
